7a50aff363b67950b3805584075a26f0ad6f069b
brianplynn <brnlynn889@gmail.com>
2018-11-15 15:49:47 -0800

    starting commit 2

--- src/components/SelectedNote.js
+++ src/components/SelectedNote.js
@@ -5,7 +5,7 @@ const SelectedNote = ({ title, body }) => {
 		return (
 			<div>
 			<div><textarea rows="1" cols="50" placeholder="Title">{title}</textarea></div>
-			<div><textarea rows="15" cols="50" placeholder="Note">{body}</textarea></div>
+			<div><textarea rows="16" cols="50" placeholder="Note">{body}</textarea></div>
 			</div>
 			)
 	
