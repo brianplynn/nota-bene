b15befeb00c5a5d41db8d0745e9e4c86cd92f701
brianplynn <brnlynn889@gmail.com>
2019-02-27 10:37:14 -0800

    responsive design

--- src/components/SelectedNote/SelectedNote.js
+++ src/components/SelectedNote/SelectedNote.js
@@ -3,12 +3,12 @@ import React from 'react';
 const SelectedNote = ({ titleChange, noteChange, title, body }) => {
 	
 		return (
-			<div className="fade-in center mw5 mw6-ns br3 hidden ba b--black-10 mv1">
-			<textarea className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3"
+			<div className="fade-in center mw5 mw7-ns br3 hidden ba b--transparent mv1 mh2">
+			<textarea className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3 w-90"
 			rows="1" cols="50" 
 			onChange={titleChange} placeholder="Title" 
 			value={title}></textarea>
-			<textarea className="f4 br3 br--top black-60 mv0 pv2 ph3"
+			<textarea className="f4 br3 br--top black-60 mv0 pv2 ph3 w-90"
 			rows="16" cols="50" 
 			onChange={noteChange} placeholder="Note"
 			value={body}></textarea>
